4e48dfe968b30db098b6c93fe674e46e4edaca21
Elsee Sea <sub01+omen@iaonline.net>
2020-12-18 02:04:24 -0600

    Successfully completed 20.5.5

---
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -0,0 +1,60 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.ViewModels;
+using CodingEventsDemo.Data;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class TagController : Controller
+    {
+        private EventDbContext context;
+
+        public TagController (EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // responds to URLs like /Tag/AddEvent/5 (where 5 is an event ID)
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId
+                };
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
+        }
+
+    } // class
+} // namespace
---
+++ CodingEventsDemo/Models/Tag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class Tag
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public Tag ()
+        {
+
+        }
+
+        public Tag (string name)
+        {
+            Name = name;
+        }
+    } // class
+} // namespace
